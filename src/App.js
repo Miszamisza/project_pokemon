327538bd42205ccffff30a8feb8246c08dc8a652
Misha <zielinskanoemi5@gmail.com>
2020-04-16 23:09:09 +0200

    package json change and add search folder

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import PokemonSuprise from "./components/PokemonSuprise";
-//import SearchPokemons from "./components/SearchPokemons";
-import "./css/App.css";
+import SearchPokemons from "./components/SearchPokemons";
 import Popup from "reactjs-popup";
+import "./css/App.css";
 
 
 export const App = () => {
-  return (
-      <>
-        <Router>
-          <NavBar/>
-          <Switch>
-            <Route exact path="/">
-                <HomePage/>
-            </Route>
-            {/*<Route exact path="/SearchPokemon">*/}
-
-            {/*</Route>*/}
-            <Route exact path="/PokemonSuprise">
-              <PokemonSuprise/>
-            </Route>
-          </Switch>
-        </Router>
-          <Popup></Popup>
-      </>
-  );
+    return (
+        <>
+            <Router>
+                <NavBar/>
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage/>
+                    </Route>
+                    <Route exact path="/SearchPokemon">
+                        <SearchPokemons/>
+                    </Route>
+                    <Route exact path="/PokemonSuprise">
+                        <PokemonSuprise/>
+                    </Route>
+                </Switch>
+            </Router>
+            <Popup></Popup>
+        </>
+    );
 };
 
 export default App;
---
+++ src/components/SearchPokemons/SearchPokemons.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {MyBox, MyButton, MyHeader} from "../../css/styles";
+import PokemonSupriseDisplay from "../../pages/PokemonSupriseDisplay";
+
+
+const SearchPokemons = () => {
+    return (
+        <div>
+            <MyHeader>Pokemon suprise for you</MyHeader>
+        </div>
+    );
+}
+
+
+export default SearchPokemons;
